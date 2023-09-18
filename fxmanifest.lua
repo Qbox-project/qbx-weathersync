f9b07d7df8c049a3bb12ac8cb328db7310d250d9
mockdot <10159765+mockdot@users.noreply.github.com>
2023-09-17 22:10:48 -0600

    fix(manifest): add ox_lib (#6)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,10 @@ description 'vSyncRevamped'
 version '2.0.0'
 
 shared_scripts {
-	'config.lua',
-	'@qb-core/shared/locale.lua',
-	'locales/en.lua'
+    'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    '@ox_lib/init.lua'
 }
 
 server_script 'server/server.lua'
